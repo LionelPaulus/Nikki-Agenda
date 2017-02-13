dbf5b404f17f5e934da2f0c30e8d8eefd7dd3336
Lionel Paulus <lionelpaulus@gmail.com>
2017-02-13 23:30:17 +0100

    ✨ Print Google auth userinfo

--- src/AppBundle/Controller/GoogleOAuthController.php
+++ src/AppBundle/Controller/GoogleOAuthController.php
@@ -24,7 +24,10 @@ class GoogleOAuthController extends Controller
         $client->getGoogleClient()->setScopes($this->accessScope);
 
         // Request access to basic informations
-        $client->getGoogleClient()->addScope('https://www.googleapis.com/auth/userinfo.email');
+        $client->getGoogleClient()->addScope(array(
+            'https://www.googleapis.com/auth/userinfo.email',
+            'https://www.googleapis.com/auth/userinfo.profile'
+        ));
 
         // Request access to offline access
         $client->getGoogleClient()->setAccessType('offline');
@@ -45,11 +48,22 @@ class GoogleOAuthController extends Controller
             $client->getGoogleClient()->setScopes($this->accessScope);
             $client->authenticate($code);
 
-            $accessToken = $client->getGoogleClient()->getAccessToken();
-            // TODO - Store the token, etc...
+            $accessToken = $client->getAccessToken();
+            // TODO: Store accessToken
+
+            $client->setAccessToken($accessToken);
+
+            $google_oauth = new \Google_Service_Oauth2($client->getGoogleClient());
+            $userinfo = $google_oauth->userinfo->get();
+
+            echo '<pre>';
+            var_dump($userinfo);
+            echo '</pre>';
+            die();
+
+            $calendar = new \Google_Service_Calendar($client->getGoogleClient()); // Works
         } else {
-            $error = $request->query->get('error');
-            // TODO - Handle the error
+            throw new \Exception("No code sent");
         }
     }
 }
