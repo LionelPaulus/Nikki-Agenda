3344555366aeaf9926f95de3ec6c03369852c651
Alex Berneau <haldir.alex@gmail.com>
2017-02-21 21:11:17 +0100

    :construction: ajax call for meeting creation

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -6,6 +6,13 @@ function outputUpdate(vol) {
 
 $(document).ready(function() {
 
+	var json;
+    $.getJSON('/scripts/data.json', function(data) {
+	    json = data;
+	    console.log(json);
+	    console.log(json.events.length)
+	});
+
     
     // Lorsque je soumets le formulaire
     $('#eventForm').on('submit', function(e) {
@@ -20,34 +27,42 @@ $(document).ready(function() {
             'to'		: $('input[name=event_dateTo]').val(),
             'duration'	: $('input[name=event_duration]').val(),
             'place'		: $('input[name=event_place]').val(),
-            'team'		: $('input[name=event_team]').val()
+            'team'		: $('select[name=event_team]').val()
         };
+        console.log(formData);
+
  
-        // Quick check
-        if(pseudo === '' || mail === '') {
-            alert('Les champs doivent êtres remplis');
-        } else {
-        	$.ajax({
-			  type: $this.attr('method'),
-			  url: $this.attr('action'),
-			  data: formData,
-			  beforeSend:function(){
-			    // this is where we append a loading image
-			    $('#ajax-panel').html('<div class="loading"><img src="/images/loading.gif" alt="Loading..." /></div>');
-			  },
-			  success:function(data){
-			    // successful request; do something with the data
-			    $('#ajax-panel').empty();
-			    $(data).find('item').each(function(i){
-			      $('#ajax-panel').append('<h4>' + $(this).find('title').text() + '</h4><p>' + $(this).find('link').text() + '</p>');
-			    });
-			  },
-			  error:function(){
-			    // failed request; give feedback to user
-			    $('#ajax-panel').html('<p class="error"><strong>Oops!</strong> Try that again in a few moments.</p>');
-			  }
-			});
-        }
+    	$.ajax({
+		  type: $this.attr('method'),
+		  url: $this.attr('action'),
+		  data: formData,
+		  dataType: 'json',
+		  beforeSend:function(){
+		  	console.log('beforeSend');
+		    // this is where we append a loading image
+		    $('.Popup.Popup--open').append('<div class="loading"><img src="/img/skin/loader1.gif" alt="Loading..." /></div>');
+		  },
+		  success:function(data){
+		  	console.log('success');
+
+
+		    // successful request; do something with the data
+		    $('#ajax-panel').empty();
+		    $(data).find('item').each(function(i){
+		      $('#ajax-panel').append('<h4>' + $(this).find('title').text() + '</h4><p>' + $(this).find('link').text() + '</p>');
+		    });
+		  },
+		  error:function(){
+
+		  	for (var i = 0; i < json.events.length; i++) {
+	  			console.log(json.events[i])
+	  		};
+
+		  	console.log('error');
+		    // failed request; give feedback to user
+		    $('#ajax-panel').html('<p class="error"><strong>Oops!</strong> Try that again in a few moments.</p>');
+		  }
+		});    
     });
 
 
