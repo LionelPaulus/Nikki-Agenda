d30b963f9db73f957ab93d87ce98fd7de8ac9a2d
clarzou <clairezunda@gmail.com>
2017-02-21 11:57:59 +0100

    :construction: fuunctional display of busy ranges

--- src/AppBundle/Controller/DisponibilitiesController.php
+++ src/AppBundle/Controller/DisponibilitiesController.php
@@ -18,43 +18,24 @@ class DisponibilitiesController extends Controller
         $accessToken = $this->get('session')->get('userGoogleAuth');
         $client->setAccessToken($accessToken);
 
+        $start_time = new \DateTime($start_time, new \DateTimeZone('Europe/Berlin'));
+        $end_time = new \DateTime($end_time, new \DateTimeZone('Europe/Berlin'));
+        $start_time = date(DATE_ATOM, strtotime($start_time->format('Y-m-d H:i:sP')));
+        $end_time = date(DATE_ATOM, strtotime($end_time->format('Y-m-d H:i:sP')));
+
         $calendar = new \Google_Service_Calendar($client->getGoogleClient());
         $list = $calendar->calendarList->listCalendarList();
-        // $start_time = $start_time->format(DateTime::ISO8601);
-        // $end_time = $end_time->format(DateTime::ISO8601);
-        //
-        // echo "<pre>";
-        //   var_dump($list);
-        // echo "</pre>";
-        // die();
-        // foreach ($list->getItems() as $calendarListEntry) {
-        //     $temp = $calendarListEntry->id;
-        //     foreach ($em as $emTemp) {
-        //         if ($temp == $emTemp) {
-        //             $idArray[$count] = $temp;
-        //             //echo $idArray[$count].'<br>';
-        //             $count++;
-        //         }
-        //     }
-        // }
 
         $freebusy = new \Google_Service_Calendar_FreeBusyRequest();
         $freebusy->setTimeMin($start_time);
         $freebusy->setTimeMax($end_time);
-        $freebusy->setTimeZone('DE');
-        // $freebusy->setItems(array($id_list));
+        $freebusy->setTimeZone('Europe/Berlin');
         $item = new \Google_Service_Calendar_FreeBusyRequestItem();
         $item->setId('primary');
         $freebusy->setItems(array($item));
         $disponibilities = $calendar->freebusy->query($freebusy);
 
-        echo "<pre>";
-          var_dump($disponibilities);
-        echo "</pre>";
+        return $disponibilities;
 
-        die('ok');
-        // return $disponibilities;
-        // $disponibilities = $this->get('app.mailer');
-        // $mailer->send('ryan@foobar.net', ...);
     }
 }
