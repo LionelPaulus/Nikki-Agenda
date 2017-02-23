51a2ba0cddab035ebcd38a8d92691f42422f8b71
Alex Berneau <haldir.alex@gmail.com>
2017-02-23 15:56:58 +0100

    :sparkles: meeting creation popup linked to home page

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -49,32 +49,51 @@ function outputUpdate(vol) {
 	document.querySelector('#duration').value = vol;
 };
 
-	var json;
-    $.getJSON('/scripts/data.json', function(data) {
-	    json = data;
-	    console.log(json);
-	    console.log(json.events.length)
-	});
+var json;
+$.getJSON('/scripts/data.json', function(data) {
+    json = data;
+    console.log(json);
+    console.log(json.events.length)
+});
 
-    
-    // Lorsque je soumets le formulaire
-    $('#formCreation').on('submit', function(e) {
-        e.preventDefault();
-        var $this = $(this); // L'objet jQuery du formulaire
-        console.log($this);
- 
-        // Je récupère les valeurs
-        var formData = {
-            'title'		: $('#formCreation input[name=event_name]').val(),
-            'from'		: $('#formCreation input[name=event_dateFrom]').val(),
-            'to'		: $('#formCreation input[name=event_dateTo]').val(),
-            'duration'	: $('#formCreation input[name=event_duration]').val(),
-            'place'		: $('#formCreation input[name=event_place]').val(),
-            'team'		: $('#formCreation select[name=event_team]').val()
-        };
-        console.log(formData);
+// Handle the pre-creation meeting input
+formPostCreation();
+function formPostCreation(){
+	$('#homeInput').on('submit', function(e) {
+	    e.preventDefault();
+	    var $this = $(this); // L'objet jQuery du formulaire
+	    console.log($this);
 
-    	$.ajax({
+	    // Je récupère les valeurs
+	    var formData = {
+	        'title'		: $('#homeInput input[name=homeInput_title]').val()
+	    };
+	    console.log(formData);
+	    $('.Popup--event input[name=event_name]').val(formData.title);
+	    $('.Popup--event').addClass('Popup--open').fadeIn();
+	    formCreation();
+	});	
+};
+
+// First step of the meeting creation
+function formCreation(){
+	$('#formCreation').on('submit', function(e) {
+	    e.preventDefault();
+	    var $this = $(this); // L'objet jQuery du formulaire
+	    console.log($this);
+
+	    // Je récupère les valeurs
+	    var formData = {
+	        'title'		: $('#formCreation input[name=event_name]').val(),
+	        'from'		: $('#formCreation input[name=event_dateFrom]').val(),
+	        'to'		: $('#formCreation input[name=event_dateTo]').val(),
+	        'duration'	: $('#formCreation input[name=event_duration]').val(),
+	        'place'		: $('#formCreation input[name=event_place]').val(),
+	        'team'		: $('#formCreation select[name=event_team]').val()
+	    };
+	    console.log(formData);
+
+		$.ajax({
 		  type: $this.attr('method'),
 		  url: $this.attr('action'),
 		  data: formData,
@@ -120,43 +139,46 @@ function outputUpdate(vol) {
 		  	console.log('error');
 		  }
 		});    
+	});
+};
+
+// Second step of the meeting creation
+function formValidation(){
+    $('#formValidation').on('submit', function(e) {
+        e.preventDefault();
+        var $this = $(this); // L'objet jQuery du formulaire
+        console.log($this);
+ 
+        // Je récupère les valeurs
+        var formData = {
+            'event'		: $('#formValidation input[name=event_id]').val()
+        };
+        console.log(formData);
+ 
+    	$.ajax({
+		  type: $this.attr('method'),
+		  url: $this.attr('action'),
+		  data: formData,
+		  dataType: 'json',
+		  beforeSend:function(){
+		    // this is where we append a loading image
+		  	console.log('beforeSend');
+		    $('.loading').fadeIn();
+		  },
+		  success:function(data){
+		    // successful request; do something with the data
+		  	console.log('success');
+		  },
+		  error:function(){
+		    // failed request; give feedback to user
+		    setTimeout(function(){
+			    $('.loading').fadeOut();
+			    $('.Popup--event').empty().addClass('Popup--validated').append('<div class="Popup--validated-title">Meeting created !</div><div class="Popup--validated-text">Every team member will receive an invite in their inboxes.</div><div class="Popup--validated-footer">Back to your <button type="button">upcoming</button> meetings.</div>');
+		    }, 1000);
+
+		  	console.log('error');
+		  }
+		});    
     });
+};
 
-    function formValidation(){
-	    $('#formValidation').on('submit', function(e) {
-	        e.preventDefault();
-	        var $this = $(this); // L'objet jQuery du formulaire
-	        console.log($this);
-	 
-	        // Je récupère les valeurs
-	        var formData = {
-	            'event'		: $('#formValidation input[name=event_id]').val()
-	        };
-	        console.log(formData);
-	 
-	    	$.ajax({
-			  type: $this.attr('method'),
-			  url: $this.attr('action'),
-			  data: formData,
-			  dataType: 'json',
-			  beforeSend:function(){
-			    // this is where we append a loading image
-			  	console.log('beforeSend');
-			    $('.loading').fadeIn();
-			  },
-			  success:function(data){
-			    // successful request; do something with the data
-			  	console.log('success');
-			  },
-			  error:function(){
-			    // failed request; give feedback to user
-			    setTimeout(function(){
-				    $('.loading').fadeOut();
-				    $('.Popup--event').empty().addClass('Popup--validated').append('<div class="Popup--validated-title">Meeting created !</div><div class="Popup--validated-text">Every team member will receive an invite in their inboxes.</div><div class="Popup--validated-footer">Back to your <button type="button">upcoming</button> meetings.</div>');
-			    }, 1000);
-
-			  	console.log('error');
-			  }
-			});    
-	    });
-    }
